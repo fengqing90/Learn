7be91591e313792a8a273a11f8e7fabd44b1c342
冯庆 <fengqing@youxin.com>
2020-10-20 16:28:42 +0800

    refine

--- learn-kafka/src/main/java/org/mfm/learn/learn_kafka/Application.java
+++ learn-kafka/src/main/java/org/mfm/learn/learn_kafka/Application.java
@@ -3,10 +3,11 @@ package org.mfm.learn.learn_kafka;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
+import javax.annotation.Resource;
+
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,7 +23,7 @@ public class Application implements CommandLineRunner {
         SpringApplication.run(Application.class, args).close();
     }
 
-    @Autowired
+    @Resource
     private KafkaTemplate<String, String> template;
 
     private final CountDownLatch latch = new CountDownLatch(3);
