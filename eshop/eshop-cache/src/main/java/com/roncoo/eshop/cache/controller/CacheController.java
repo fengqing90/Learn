3b677fd88ba50c6ebe35f73d8c9811b5a7568663
冯庆 <fengqing@youxin.com>
2020-11-03 17:27:43 +0800

    亿级流量电商详情页系统实战：55_基于nginx+lua+java完成多级缓存架构的核心业务逻辑（二）

--- eshop/eshop-cache/src/main/java/com/roncoo/eshop/cache/controller/CacheController.java
+++ eshop/eshop-cache/src/main/java/com/roncoo/eshop/cache/controller/CacheController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.roncoo.eshop.cache.model.ProductInfo;
+import com.roncoo.eshop.cache.model.ShopInfo;
 import com.roncoo.eshop.cache.service.CacheService;
 
 /**
@@ -31,6 +32,46 @@ public class CacheController {
 	@ResponseBody
 	public ProductInfo testGetCache(Long id) {
 		return cacheService.getLocalCache(id);
+	}
+	
+	@RequestMapping("/getProductInfo")
+	@ResponseBody
+	public ProductInfo getProductInfo(Long productId) {
+		ProductInfo productInfo = null;
+		
+		productInfo = cacheService.getProductInfoFromReidsCache(productId);
+		System.out.println("=================从redis中获取缓存，商品信息=" + productInfo);   
+		
+		if(productInfo == null) {
+			productInfo = cacheService.getProductInfoFromLocalCache(productId);
+			System.out.println("=================从ehcache中获取缓存，商品信息=" + productInfo);   
+		}
+		
+		if(productInfo == null) {
+			// 就需要从数据源重新拉去数据，重建缓存，但是这里先不讲
+		}
+		
+		return productInfo;
+	}
+	
+	@RequestMapping("/getShopInfo")
+	@ResponseBody
+	public ShopInfo getShopInfo(Long shopId) {
+		ShopInfo shopInfo = null;
+		
+		shopInfo = cacheService.getShopInfoFromReidsCache(shopId);
+		System.out.println("=================从redis中获取缓存，店铺信息=" + shopInfo);   
+		
+		if(shopInfo == null) {
+			shopInfo = cacheService.getShopInfoFromLocalCache(shopId);
+			System.out.println("=================从ehcache中获取缓存，店铺信息=" + shopInfo);   
+		}
+		
+		if(shopInfo == null) {
+			// 就需要从数据源重新拉去数据，重建缓存，但是这里先不讲
+		}
+		
+		return shopInfo;
 	}
 	
 }
