7f710323b3f6ae956dce2d18b5f01ad62a7dfcc1
冯庆 <fengqing@youxin.com>
2020-11-06 19:38:48 +0800

    亿级流量电商详情页系统实战：116_为源头服务的限流场景增加stubbed fallback降级机制

--- eshop/eshop-cache/src/main/java/com/roncoo/eshop/cache/hystrix/command/GetProductInfoCommand.java
+++ eshop/eshop-cache/src/main/java/com/roncoo/eshop/cache/hystrix/command/GetProductInfoCommand.java
@@ -25,8 +25,41 @@ public class GetProductInfoCommand extends HystrixCommand<ProductInfo> {
 	
 	@Override
 	protected ProductInfo run() throws Exception {
+		// 发送http或rpc接口调用，去调用商品服务的接口
 		String productInfoJSON = "{\"id\": " + productId + ", \"name\": \"iphone7手机\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 1, \"modifiedTime\": \"2017-01-01 12:01:00\"}";
 		return JSONObject.parseObject(productInfoJSON, ProductInfo.class);
 	} 
+	
+	@Override
+	protected ProductInfo getFallback() {
+		HBaseColdDataCommand command = new HBaseColdDataCommand(productId);
+		return command.execute();
+	}
+	
+	private class HBaseColdDataCommand extends HystrixCommand<ProductInfo> {
+
+		private Long productId;
+		
+		public HBaseColdDataCommand(Long productId) {
+			super(HystrixCommandGroupKey.Factory.asKey("HBaseGroup"));
+			this.productId = productId;
+		}
+		
+		@Override
+		protected ProductInfo run() throws Exception {
+			// 查询hbase
+			String productInfoJSON = "{\"id\": " + productId + ", \"name\": \"iphone7手机\", \"price\": 5599, \"pictureList\":\"a.jpg,b.jpg\", \"specification\": \"iphone7的规格\", \"service\": \"iphone7的售后服务\", \"color\": \"红色,白色,黑色\", \"size\": \"5.5\", \"shopId\": 1, \"modifiedTime\": \"2017-01-01 12:01:00\"}";
+			return JSONObject.parseObject(productInfoJSON, ProductInfo.class);
+		}
+		
+		@Override
+		protected ProductInfo getFallback() {
+			ProductInfo productInfo = new ProductInfo();
+			productInfo.setId(productId);
+			// 从内存中找一些残缺的数据拼装进去
+			return productInfo;
+		}
+		
+	}
 
 }
