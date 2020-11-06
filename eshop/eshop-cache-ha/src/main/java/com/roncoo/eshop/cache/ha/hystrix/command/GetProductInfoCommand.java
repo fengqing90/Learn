c1aad11569f5e865dd00fa7b4153c6754be3873f
冯庆 <fengqing@youxin.com>
2020-11-06 10:37:38 +0800

    亿级流量电商详情页系统实战：97_深入理解线程池隔离技术的设计原则以及动手实战接口限流实验

--- eshop/eshop-cache-ha/src/main/java/com/roncoo/eshop/cache/ha/hystrix/command/GetProductInfoCommand.java
+++ eshop/eshop-cache-ha/src/main/java/com/roncoo/eshop/cache/ha/hystrix/command/GetProductInfoCommand.java
@@ -24,12 +24,15 @@ public class GetProductInfoCommand extends HystrixCommand<ProductInfo> {
 				.andCommandKey(HystrixCommandKey.Factory.asKey("GetProductInfoCommand"))
 				.andThreadPoolKey(HystrixThreadPoolKey.Factory.asKey("GetProductInfoPool"))
 				.andThreadPoolPropertiesDefaults(HystrixThreadPoolProperties.Setter()
-						.withCoreSize(15)
-						.withQueueSizeRejectionThreshold(10))
+						.withCoreSize(10)
+						.withMaxQueueSize(12)
+						.withQueueSizeRejectionThreshold(15)) 
 				.andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
 						.withCircuitBreakerRequestVolumeThreshold(30)
 						.withCircuitBreakerErrorThresholdPercentage(40)
-						.withCircuitBreakerSleepWindowInMilliseconds(3000))
+						.withCircuitBreakerSleepWindowInMilliseconds(3000)
+						.withExecutionTimeoutInMilliseconds(20000)
+						.withFallbackIsolationSemaphoreMaxConcurrentRequests(30))  
 				);  
 		this.productId = productId;
 	}
@@ -40,6 +43,10 @@ public class GetProductInfoCommand extends HystrixCommand<ProductInfo> {
 		
 		if(productId.equals(-1L)) {
 			throw new Exception();
+		}
+		
+		if(productId.equals(-2L)) {
+			Thread.sleep(3000);  
 		}
 		
 		String url = "http://127.0.0.1:8082/getProductInfo?productId=" + productId;
