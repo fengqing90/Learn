f25e25596df822bb9b40bb45886a1838c316ca2d
冯庆 <fengqing@youxin.com>
2020-11-06 10:14:34 +0800

    亿级流量电商详情页系统实战：96_深入理解hystrix的短路器原理以及接口异常时的熔断实验

--- eshop/eshop-cache-ha/src/main/java/com/roncoo/eshop/cache/ha/hystrix/command/GetProductInfoCommand.java
+++ eshop/eshop-cache-ha/src/main/java/com/roncoo/eshop/cache/ha/hystrix/command/GetProductInfoCommand.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
 import com.netflix.hystrix.HystrixCommandKey;
+import com.netflix.hystrix.HystrixCommandProperties;
 import com.netflix.hystrix.HystrixThreadPoolKey;
 import com.netflix.hystrix.HystrixThreadPoolProperties;
 import com.roncoo.eshop.cache.ha.http.HttpClientUtils;
@@ -25,15 +26,24 @@ public class GetProductInfoCommand extends HystrixCommand<ProductInfo> {
 				.andThreadPoolPropertiesDefaults(HystrixThreadPoolProperties.Setter()
 						.withCoreSize(15)
 						.withQueueSizeRejectionThreshold(10))
+				.andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
+						.withCircuitBreakerRequestVolumeThreshold(30)
+						.withCircuitBreakerErrorThresholdPercentage(40)
+						.withCircuitBreakerSleepWindowInMilliseconds(3000))
 				);  
 		this.productId = productId;
 	}
 	
 	@Override
 	protected ProductInfo run() throws Exception {
+		System.out.println("调用接口，查询商品数据，productId=" + productId); 
+		
+		if(productId.equals(-1L)) {
+			throw new Exception();
+		}
+		
 		String url = "http://127.0.0.1:8082/getProductInfo?productId=" + productId;
 		String response = HttpClientUtils.sendGetRequest(url);
-		System.out.println("调用接口，查询商品数据，productId=" + productId); 
 		return JSONObject.parseObject(response, ProductInfo.class);  
 	}
 	
@@ -41,5 +51,12 @@ public class GetProductInfoCommand extends HystrixCommand<ProductInfo> {
 //	protected String getCacheKey() {
 //		return "product_info_" + productId;
 //	}
+	
+	@Override
+	protected ProductInfo getFallback() {
+		ProductInfo productInfo = new ProductInfo();
+		productInfo.setName("降级商品");  
+		return productInfo;
+	}
 	
 }
