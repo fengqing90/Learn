b66e772a338a716077b2ebd043f2e10f832eecab
冯庆 <fengqing@youxin.com>
2020-11-09 15:04:34 +0800

    155_商品详情页动态渲染系统：Spring Cloud之Config统一配置中心

--- eshop-v2/greeting-service/src/main/java/com/roncoo/eshop/web/controller/GreetingController.java
+++ eshop-v2/greeting-service/src/main/java/com/roncoo/eshop/web/controller/GreetingController.java
@@ -1,6 +1,7 @@
 package com.roncoo.eshop.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,25 @@ public class GreetingController {
 	@Autowired
 	private SayHelloService sayHelloService;
 	
+	@Value("${server.port}")
+	private String port;
+	
+	@Value("${defaultName}")
+	private String defaultName;
+	
 	@RequestMapping("/greeting")
 	public String greeting(@RequestParam String name) {
-		return sayHelloService.sayHello(name);
+		String result = null;
+		
+		if(name != null && !"".equals(name)) { 
+			result = sayHelloService.sayHello(name);
+		} else {
+			result = "hello, this is default name: " + defaultName;
+		}
+		
+		result += " , through greeting service from port: " + port;
+		
+		return result;
 	}
 	
 }
