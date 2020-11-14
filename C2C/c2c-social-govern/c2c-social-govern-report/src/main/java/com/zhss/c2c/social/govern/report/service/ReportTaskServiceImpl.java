54e30c098640879e388a970bf492151fb8a5aa8c
冯庆 <fengqing@youxin.com>
2020-11-14 15:46:34 +0800

    20_基于Dubbo开发C2C电商社会化治理平台任务服务

---
+++ C2C/c2c-social-govern/c2c-social-govern-report/src/main/java/com/zhss/c2c/social/govern/report/service/ReportTaskServiceImpl.java
@@ -0,0 +1,29 @@
+package com.zhss.c2c.social.govern.report.service;
+
+import com.zhss.c2c.social.govern.report.dao.ReportTaskDAO;
+import com.zhss.c2c.social.govern.report.domain.ReportTask;
+import com.zhss.c2c.social.govern.report.mapper.ReportTaskMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ * 举报任务Service实现类
+ */
+@Repository
+public class ReportTaskServiceImpl implements ReportTaskService {
+
+    /**
+     * 举报任务DAO组件
+     */
+    @Autowired
+    private ReportTaskDAO reportTaskDAO;
+
+    /**
+     * 增加举报任务
+     * @param reportTask 举报任务
+     */
+    @Override
+    public void add(ReportTask reportTask) {
+        reportTaskDAO.add(reportTask);
+    }
+}
