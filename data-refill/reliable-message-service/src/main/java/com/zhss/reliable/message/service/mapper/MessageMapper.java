0033c7beb7279db4efbfc8d17b993f6843013b66
冯庆 <fengqing@youxin.com>
2020-11-19 18:11:20 +0800

    99_完成可靠消息服务的开发（二）：完成正常流程下的接口与功能开发

--- data-refill/reliable-message-service/src/main/java/com/zhss/reliable/message/service/mapper/MessageMapper.java
+++ data-refill/reliable-message-service/src/main/java/com/zhss/reliable/message/service/mapper/MessageMapper.java
@@ -1,7 +1,5 @@
 package com.zhss.reliable.message.service.mapper;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
@@ -52,25 +50,10 @@ public interface MessageMapper {
 	})
 	public Message findById(@Param("id") Long id);
 	
-	@Select("SELECT * FROM message WHERE status=#{status}")     
-	@Results({
-		@Result(id = true, column = "id", property = "id"),
-		@Result(column = "content", property = "content"),
-		@Result(column = "status", property = "status"),
-		@Result(column = "created_time", property = "createdTime"),
-		@Result(column = "removed_time", property = "removedTime"),
-		@Result(column = "confirmed_time", property = "confirmedTime"),
-		@Result(column = "finished_time", property = "finishedTime") 
-	})
-	public List<Message> findByStatus(@Param("status") Integer status); 
-	
 	@Update("UPDATE message SET "
 			+ "status=#{status},"
 			+ "finished_time=#{finishedTime} "
 		+ "WHERE id=#{id}")  
 	public void finish(Message message); 
-	
-	@Update("UPDATE message SET content=#{content} WHERE id=#{id}")  
-	public void updateContent(Message message);
 	
 }
