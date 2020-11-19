2284468e565924b6234fe04cbeb5856ee1fdee37
冯庆 <fengqing@youxin.com>
2020-11-19 18:00:51 +0800

    63_流量充值中心重构（六）：为流量充值总控服务引入TCC分布式事务

--- data-refill/data-refill-center-service/src/main/java/com/zhss/data/refill/center/DataRefillCenterServiceApplication.java
+++ data-refill/data-refill-center-service/src/main/java/com/zhss/data/refill/center/DataRefillCenterServiceApplication.java
@@ -5,13 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.context.annotation.ImportResource;
 
+/**
+ * 活动服务
+ * @author zhonghuashishan
+ *
+ */
 @SpringBootApplication
 @ServletComponentScan
 @EnableEurekaClient
 @EnableFeignClients
-@ImportResource({ "classpath:bytetcc-supports-springcloud.xml" })  
 public class DataRefillCenterServiceApplication {
 	
 	public static void main(String[] args) { 
