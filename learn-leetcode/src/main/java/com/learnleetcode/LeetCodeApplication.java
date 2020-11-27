380e89934ded98f78a3beb9811abfd2c48ad8ff2
冯庆 <fengqing@youxin.com>
2020-11-27 18:43:04 +0800

    测试

--- learn-leetcode/src/main/java/com/learnleetcode/LeetCodeApplication.java
+++ learn-leetcode/src/main/java/com/learnleetcode/LeetCodeApplication.java
@@ -2,15 +2,23 @@ package com.learnleetcode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author fengqing
  */
 @SpringBootApplication
+@RestController
 public class LeetCodeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LeetCodeApplication.class, args);
     }
 
+    @RequestMapping("/{name}")
+    public String test(@PathVariable String name) {
+        return "hello " + name;
+    }
 }
