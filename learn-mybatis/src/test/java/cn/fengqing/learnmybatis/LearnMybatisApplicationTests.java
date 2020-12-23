9f0bd45985c4ee512c20289cdd3ce130e2af2e64
冯庆 <fengqing@youxin.com>
2020-12-23 14:43:37 +0800

    init learn-mybatis

---
+++ learn-mybatis/src/test/java/cn/fengqing/learnmybatis/LearnMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fengqing.learnmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
