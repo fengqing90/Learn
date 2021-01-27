d931a16b8b2a12bb2fef650f3b1fdaaa77675163
冯庆 <fengqing@youxin.com>
2021-01-27 11:03:30 +0800

    Learn-mybatis : 自动生成

---
+++ src/main/java/cn/fengqing/learnmybatis/generate/model/mapper/CityMapper.java
@@ -0,0 +1,16 @@
+package cn.fengqing.learnmybatis.generate.model.mapper;
+
+import cn.fengqing.learnmybatis.generate.model.entity.City;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author fengqing
+ * @since 2021-01-19
+ */
+public interface CityMapper extends BaseMapper<City> {
+
+}
---
+++ src/main/java/cn/fengqing/learnmybatis/generate/model/service/CityService.java
@@ -0,0 +1,16 @@
+package cn.fengqing.learnmybatis.generate.model.service;
+
+import cn.fengqing.learnmybatis.generate.model.entity.City;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author fengqing
+ * @since 2021-01-19
+ */
+public interface CityService extends IService<City> {
+
+}
