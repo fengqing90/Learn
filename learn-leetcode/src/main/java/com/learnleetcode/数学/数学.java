17b89e5614bfb2416385098af14340de0a149b2f
冯庆 <fengqing@youxin.com>
2021-01-27 11:02:48 +0800

    LeetCode： 202. 快乐数

--- learn-leetcode/src/main/java/com/learnleetcode/数学/数学.java
+++ learn-leetcode/src/main/java/com/learnleetcode/数学/数学.java
@@ -15,6 +15,56 @@ class 数学 extends LeetCode {
         Excel表列名称.run();
         Excel表列序号.run();
         阶乘后的零.run();
+        快乐数.run();
+    }
+
+    /**
+     * 202. 快乐数
+     * 
+     * <pre>
+     * 编写一个算法来判断一个数 n 是不是快乐数。
+     *
+     * 「快乐数」定义为：对于一个正整数，每一次将该数替换为它每个位置上的数字的平方和，然后重复这个过程直到这个数变为 1，也可能是 无限循环 但始终变不到 1。如果 可以变为  1，那么这个数就是快乐数。
+     *
+     * 如果 n 是快乐数就返回 True ；不是，则返回 False 。
+     *
+     * 示例：
+     * 输入：19
+     * 输出：true
+     * 解释：
+     * 12 + 92 = 82
+     * 82 + 22 = 68
+     * 62 + 82 = 100
+     * 12 + 02 + 02 = 1
+     * </pre>
+     */
+
+    final static class 快乐数 extends 数学 {
+
+        static void run() {
+            快乐数 快乐数 = new 快乐数();
+            System.out.println(快乐数.isHappy(2));
+        }
+
+        public boolean isHappy(int n) {
+            int slow = n, fast = squareSum(n);
+            while (slow != fast) {
+                slow = squareSum(slow);
+                fast = squareSum(fast);
+                fast = squareSum(fast);
+            }
+            return slow == 1;
+        }
+
+        public int squareSum(int n) {
+            int sum = 0;
+            while (n > 0) {
+                int digit = n % 10;
+                sum += digit * digit;
+                n = n / 10;
+            }
+            return sum;
+        }
     }
 
     /**
