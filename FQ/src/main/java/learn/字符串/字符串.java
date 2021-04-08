c8a2d22060d3bdba23862cebd8916bf970221b34
冯庆 <fengqing@youxin.com>
2021-04-08 16:15:26 +0800

    Learn  : 字符串

--- FQ/src/main/java/learn/字符串/字符串.java
+++ FQ/src/main/java/learn/字符串/字符串.java
@@ -1,7 +1,7 @@
 package learn.字符串;
 
 /**
- * TODO
+ * https://mp.weixin.qq.com/s?__biz=MzUxMDk2OTAzMQ==&mid=2247483998&idx=1&sn=646731ccdfb58986893544e8a5609336&chksm=f97b97f6ce0c1ee071ab4212680072ea9f1d4e5f1669073f513829de98394e96958a54ec365a&token=756465261&lang=zh_CN#rd
  *
  * @author fengqing
  * @date 2021/4/8 14:14
@@ -18,11 +18,16 @@ public class 字符串 {
     private static void test() {
         String s0 = "kvill";
         String s00 = "kvill";
-        System.out.println(s0 == s00);
+        String s000 = "kv" + "ill";
+        System.out.println(s0 == s00);              // true
+        System.out.println(s0 == ("kv" + "ill"));   // true
+        System.out.println(s0 == s000);             // true
 
         String s1 = new String("kvill");
-        String s2 = new String("kvill");
         System.out.println(s0 == s1); // false
+
+        String s2 = new String("kvill");
+        System.out.println(s1 == s2); // false
 
         s2 = s2.intern();
         System.out.println(s0 == s1); // false
