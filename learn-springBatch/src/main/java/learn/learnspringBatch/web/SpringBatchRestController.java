43932d863215d050c0039e309f12bf036a8a3bfc
冯庆 <fengqing@youxin.com>
2021-04-21 19:13:34 +0800

    refine

--- learn-springBatch/src/main/java/learn/learnspringBatch/web/SpringBatchRestController.java
+++ learn-springBatch/src/main/java/learn/learnspringBatch/web/SpringBatchRestController.java
@@ -1,10 +1,9 @@
 package learn.learnspringBatch.web;
 
-import org.apache.http.client.utils.DateUtils;
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Date;
 
 /**
  * @ClassName SpringBatchRestController
@@ -15,7 +14,7 @@ import java.util.Date;
 public class SpringBatchRestController {
 
     @GetMapping("/")
-    public String info() {
-        return DateUtils.formatDate(new Date());
+    public Object info() {
+        return new Date();
     }
 }
