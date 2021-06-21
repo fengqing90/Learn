e0e4aca385b5de8ef6bf64d2f90d6caeedbb9bea
冯庆 <fengqing@youxin.com>
2021-06-21 15:18:02 +0800

    refine

--- learn-rabbitMQ/src/main/java/learn/rabbitmq/demo/SendTest.java
+++ learn-rabbitMQ/src/main/java/learn/rabbitmq/demo/SendTest.java
@@ -1,19 +1,20 @@
 package learn.rabbitmq.demo;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.concurrent.TimeoutException;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.lang3.time.DateFormatUtils;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
-import org.apache.commons.lang3.RandomStringUtils;
-import org.apache.commons.lang3.time.DateFormatUtils;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.support.CorrelationData;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-
-import java.io.IOException;
-import java.util.Date;
-import java.util.concurrent.TimeoutException;
 
 /**
  * @ClassName SendTest
@@ -30,13 +31,15 @@ public class SendTest {
     private static final String exchange = "ex_test";
     private static final String key = "key_test";
 
-    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+    public static void main(String[] args)
+            throws IOException, TimeoutException, InterruptedException {
 //        doAmpqSend();
 
         doSpringSend();
     }
 
-    private static void doAmpqSend() throws IOException, TimeoutException, InterruptedException {
+    private static void doAmpqSend()
+            throws IOException, TimeoutException, InterruptedException {
 
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(host);
@@ -48,9 +51,10 @@ public class SendTest {
         Channel channel = connection.createChannel();
 
         String message = "RabbitMQ Demo Test:"
-                + DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
+            + DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
 
-        channel.basicPublish(exchange, key, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
+        channel.basicPublish(exchange, key,
+            MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
 
         Thread.sleep(2000);
         channel.close();
@@ -66,7 +70,8 @@ public class SendTest {
         connectionFactory.setPassword(accounting);
         connectionFactory.setVirtualHost(vhost_fq);
         connectionFactory.setChannelCacheSize(100);
-        connectionFactory.setCacheMode(CachingConnectionFactory.CacheMode.CHANNEL);
+        connectionFactory
+            .setCacheMode(CachingConnectionFactory.CacheMode.CHANNEL);
 
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMandatory(true);
@@ -74,10 +79,10 @@ public class SendTest {
         rabbitTemplate.setRoutingKey(key);
         rabbitTemplate.setMessageConverter(new Jackson2JsonMessageConverter());
 
-
         String message = "RabbitMQ Demo Test:"
-                + DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
-        rabbitTemplate.correlationConvertAndSend(message, new CorrelationData(RandomStringUtils.random(1)));
+            + DateFormatUtils.format(new Date(), "yyyy-MM-dd HH:mm:ss");
+        rabbitTemplate.correlationConvertAndSend(message,
+            new CorrelationData(RandomStringUtils.random(1)));
 
         System.out.println("doSpringSend end.");
         connectionFactory.destroy();
