02bb73ea7588e282c50801dc8fd6a414da8079e9
冯庆 <fengqing@youxin.com>
2021-08-04 11:55:46 +0800

    Learn  : leetcode

--- learn-leetcode/src/main/java/com/learnleetcode/ali/Ali.java
+++ learn-leetcode/src/main/java/com/learnleetcode/ali/Ali.java
@@ -17,6 +17,62 @@ import com.learnleetcode.LeetCode;
 public class Ali extends LeetCode {
 
     /**
+     * n个物品每个物品都有一定价值，分给2个人，怎么分两个人的价值差最小
+     */
+    final static class 背包问题_变种_2人分最小差值 extends Ali {
+        public static void main(String[] args) {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            int value = new 背包问题_变种_2人分最小差值().match(arr);
+            System.out.println(value);
+        }
+
+        public int match(int[] arr) {
+            int sum = Arrays.stream(arr).sum();
+            int len = arr.length;
+            int halfOfSum = sum / 2;
+            // 确定矩阵二维定义：
+            // 第一维代表前i个物体，i可为0；
+            // 第二维代表从0开始的连续容量值确定矩阵长宽，并初始化。
+            // 因为矩阵第一维和第二维都是从0开始，所以要加一
+            int matrix_firstDimensionLen = len + 1;
+            int matrix_secondDimensionLen = halfOfSum + 1;
+            int[][] matrix = new int[matrix_firstDimensionLen][matrix_secondDimensionLen];
+            //初始化矩阵边界为0
+            Arrays.fill(matrix[0], 0);
+
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+
+            //arr的下标，是否与matrix的下标冲突:是的
+            //matrix[i][j]定义：用前i个物体装容量为j的背包能够装下的最大值
+            //arr[i]定义：第i+1个物体的大小，所以arr[i-1]才是第i个物体的大小
+            //遍历从矩阵边界开始（不包括边界），所以i = 1， j = 1
+            for (int i = 1; i < matrix_firstDimensionLen; i++) {
+                for (int j = 1; j < matrix_secondDimensionLen; j++) {
+                    //如果第i件物体不装进背包
+                    matrix[i][j] = matrix[i - 1][j];
+                    //如果第i件物体装进背包     //备注：j - arr[i-1] >= 0防止下标为负
+                    int arr_i = arr[i - 1];
+                    if (j - arr_i >= 0
+                        && matrix[i - 1][j - arr_i] + arr_i > matrix[i][j]) {
+                        matrix[i][j] = matrix[i - 1][j - arr_i] + arr_i;
+                    }
+                }
+            }
+            for (int[] ints : matrix) {
+                for (int j = 0; j < ints.length; j++) {
+                    System.out.print(ints[j] + "\t");
+                }
+                System.out.println("");
+            }
+            System.out.println(matrix[len][halfOfSum]);
+            return sum - matrix[len][halfOfSum] * 2;
+        }
+
+    }
+
+    /**
      * 121. 买卖股票的最佳时机
      * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
      */
--- learn-leetcode/src/main/java/com/learnleetcode/其他/其他.java
+++ learn-leetcode/src/main/java/com/learnleetcode/其他/其他.java
@@ -14,6 +14,163 @@ public class 其他 extends LeetCode {
         生兔子.run();
     }
 
+    static class 小顶堆 {
+
+        public static void main(String[] args) {
+            int[] array = new int[] { 7, 5, 15, 3, 17, 2, 20, 24, 1, 9, 12, 8 };
+            System.out.println(findNumberK(array, 5));
+        }
+
+        /**
+         * 寻找第k大元素
+         * 
+         * @param array
+         *        待调整的数组
+         * @param k
+         *        第几大
+         * @return
+         */
+        public static int findNumberK(int[] array, int k) {
+            //1.用前k个元素构建小顶堆
+            buildHeap(array, k);
+            //2.继续遍历数组，和堆顶比较
+            for (int i = k; i < array.length; i++) {
+                if (array[i] > array[0]) {
+                    array[0] = array[i];
+                    downAdjust(array, 0, k);
+                }
+            }
+            //3.返回堆顶元素
+            return array[0];
+        }
+
+        private static void buildHeap(int[] array, int length) {
+            //从最后一个非叶子节点开始，依次下沉调整
+            for (int i = (length - 2) / 2; i >= 0; i--) {
+                downAdjust(array, i, length);
+            }
+        }
+
+        /**
+         * 下沉调整
+         * 
+         * @param array
+         *        待调整的堆
+         * @param index
+         *        要下沉的节点
+         * @param length
+         *        堆的有效大小
+         */
+        private static void downAdjust(int[] array, int index, int length) {
+            //temp保存父节点的值，用于最后的赋值
+            int temp = array[index];
+            int childIndex = 2 * index + 1;
+            while (childIndex < length) {
+                //如果有右孩子，且右孩子小于左孩子的值，则定位到右孩子
+                if (childIndex + 1 < length
+                    && array[childIndex + 1] < array[childIndex]) {
+                    childIndex++;
+                }
+                //如果父节点小于任何一个孩子的值，直接跳出
+                if (temp <= array[childIndex])
+                    break;
+                //无需真正交换，单项赋值即可
+                array[index] = array[childIndex];
+                index = childIndex;
+                childIndex = 2 * childIndex + 1;
+            }
+            array[index] = temp;
+        }
+
+    }
+
+    static class Solution extends 其他 {
+        public static void main(String[] args) {
+            int[] array = new int[] { 92, 5, 88, 13, 80, 11, 23, 666, 432 };
+            System.out.println(new Solution().findKLargest(array, 2));
+        }
+
+        public int findKLargest(int[] nums, int k) {
+            return findK(nums, k, 0, nums.length - 1);
+        }
+
+        private int findK(int[] nums, int k, int start, int end) {
+            int low = start;
+            int high = end;
+            int temp = nums[low];// 枢轴点
+            while (low < high) {
+                while (low < high && temp >= nums[high]) {
+                    high--;
+                }
+                // 遇到高位比temp小，把高位放到地位
+                nums[low] = nums[high];
+
+                while (low < high && temp <= nums[low]) {
+                    low++;
+                }
+                // 遇到低位比temp大，把地位放到高位
+                nums[high] = nums[low];
+            }
+            // 曲轴点归位
+            nums[high] = temp;
+
+            if (high == k - 1) {
+                return temp; // 如果当前是第k大的数据直接返回
+            } else if (high > k - 1) {
+                return findK(nums, k, start, high - 1);
+            } else {
+                return findK(nums, k, high + 1, end);
+            }
+        }
+    }
+
+    /**
+     * 如何寻找无序数组中的第K大元素？
+     * 快速排序
+     */
+    public static class disorderSearchBin extends 其他 {
+
+        public int quickSortOneTime(int[] array, int low, int high) { //一趟快速排序   
+            int key = array[low];
+            while (low < high) {
+                while (key < array[high] && low < high)
+                    high--;
+
+                array[low] = array[high];
+
+                while (key > array[low] && low < high)
+                    low++;
+
+                array[high] = array[low];
+            }
+            array[high] = key;
+            return high;
+        }
+
+        public int Select_k(int[] array, int low, int high, int k) {
+            int index;
+            if (low == high)
+                return array[low];
+            int partition = quickSortOneTime(array, low, high);
+            index = high - partition + 1;  //找到的是第几个大值
+            if (index == k) {
+                return array[partition];
+            } else if (index < k) {//此时向左查找
+                return Select_k(array, low, partition - 1, k - index);  //查找的是相对位置的值，k在左段中的下标为k-index
+            } else {
+                return Select_k(array, partition + 1, high, k);
+            }
+        }
+
+        public static void main(String[] args) {
+            int[] array = new int[] { 92, 5, 88, 13, 80 };
+            int index = new disorderSearchBin().Select_k(array, 0,
+                array.length - 1, 2);
+            System.out.print(index);
+        }
+
+    }
+
     /**
      * 古典问题：有一对兔子，从出生后第3个月起每个月都生一对兔子，小兔子长到第三个月后每个月又生一对兔子，假如兔子都不死，问每个月的兔子总数为多少？
      */
